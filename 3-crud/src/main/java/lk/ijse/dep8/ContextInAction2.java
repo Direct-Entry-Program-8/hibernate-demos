6890d6b80fa4b9ba8ea31899e9eb66797380d459
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-05-30 16:50:18 +0530

    Context in Action - 3

---
+++ 3-crud/src/main/java/lk/ijse/dep8/ContextInAction2.java
@@ -0,0 +1,35 @@
+package lk.ijse.dep8;
+
+import lk.ijse.dep8.entity.Customer;
+import lk.ijse.dep8.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class ContextInAction2 {
+
+    public static void main(String[] args) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        try {
+            session.beginTransaction();
+
+            Customer dulanga = session.get(Customer.class, 2);
+            dulanga.setAddress("Panadura");
+            System.out.println(session.contains(dulanga));  // Yes
+            session.detach(dulanga);
+            System.out.println(session.contains(dulanga));  // No
+            session.merge(dulanga);
+
+            session.getTransaction().commit();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            if (session != null && session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+        }finally{
+            session.close();
+            sf.close();
+        }
+    }
+}
